47e909ffcfc2df0153bd40d143b1f0ee91c255dc
onlysea2704 <phamduyhai2704@gmail.com>
2025-07-27 21:26:47 +0700

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./Components/NavBar/NavBar";
+import LoginForm from "./pages/LoginForm/LoginForm";
+import RegisterForm from "./pages/RegisterForm/RegisterForm";
+// import CategoryCourse from "./Pages/CategoryCourse/CategoryCourse";
+// import CourseDetail from "./Pages/CourseDetail/CourseDetail";
+// import Home from "./Pages/Home/Home";
+// import Lesson from "./Pages/Lesson/Lesson";
+// import ForgetPassword from "./Pages/ChangePassword/ChangePassword";
+// import ProfileEdit from "./Pages/ProfileEdit/ProfileEdit";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/lesson" element={<Lesson />} />
+        <Route path="/my-course" element={<CategoryCourse isPurchase={true}/>} />
+        <Route path="/explore-course" element={<CategoryCourse isPurchase={false}/>} /> */}
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        {/* <Route path="/coursedetail">
+          <Route path=":id_course" element={<CourseDetail />} />
+          <Route path=":id_course/lesson/:id_lesson" element={<Lesson />} />
+        </Route>
+        <Route path="/change-password" element={<ChangePassword />} />
+        <Route path="/profile-edit" element={<ProfileEdit />} />
+        <Route path="/progress/:id_course" element={<Progress />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/RegisterForm/RegisterForm.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "./LoginForm.css";
+import { Link, useNavigate } from "react-router-dom";
+
+const RegisterForm = () => {
+
+    const [index, setIndex] = useState(0);
+
+    const texts = [
+        {
+            title: "Giải Pháp Vượt Trội...",
+            content:
+                "Sự hài lòng của Quý khách hàng chính là động lực để chúng tôi không ngừng cải tiến và nâng cao chất lượng dịch vụ.",
+        },
+        {
+            title: "Chuyên Nghiệp Dẫn Đầu",
+            content:
+                "We-tech tự hào đồng hành cùng quý khách hàng trên hành trình phát triển bền vững.",
+        },
+    ];
+
+    return (
+        <div className="login-background">
+            <div className="login-wrapper">
+                <div className="login-left">
+                    <div className="text-slider">
+                        <h2>{texts[index].title}</h2>
+                        <p>{texts[index].content}</p>
+                        <div className="slider-dots">
+                            {texts.map((_, i) => (
+                                <span
+                                    key={i}
+                                    className={`dot ${i === index ? "active" : ""}`}
+                                    onClick={() => setIndex(i)}
+                                ></span>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+                <div className="login-right">
+                    <div className="login-box">
+                        <h2>Đăng Nhập</h2>
+                        <p>
+                            Bạn chưa có tài khoản? <a href="#">Đăng Ký</a>
+                        </p>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Mật khẩu" />
+                        <div class="name-row">
+                            <input type="text" placeholder="Họ" class="name-input" />
+                            <input type="text" placeholder="Tên" class="name-input" />
+                        </div>
+                        <p className="forgot-password">Quên mật khẩu?</p>
+                        <button className="btn-login">Đăng Nhập</button>
+                        <div className="divider">
+                            <span className="line"></span>
+                            <span>or</span>
+                            <span className="line"></span>
+                        </div>
+                        <button className="btn-google">
+                            <img
+                                src="https://developers.google.com/identity/images/g-logo.png"
+                                alt="Google"
+                            />
+                            Đăng nhập với Google
+                        </button>
+                        <p className="terms">
+                            Protected by reCAPTCHA and subject to the Prism{" "}
+                            <a href="#">Privacy Policy</a> and{" "}
+                            <a href="#">Terms of Service</a>.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterForm;
