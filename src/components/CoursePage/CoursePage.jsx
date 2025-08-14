3db245337b915033447d9387ece8f503bb07483f
onlysea2704 <phamduyhai2704@gmail.com>
2025-08-15 01:54:27 +0700

    m

--- src/components/CoursePage/CoursePage.jsx
+++ src/components/CoursePage/CoursePage.jsx
@@ -2,32 +2,35 @@ import React, { useState } from 'react';
 import './CoursePage.css';
 import CourseContent from '../CourseContent/CourseContent';
 import CourseIntro from '../CourseIntro/CourseIntro';
+import Faq from '../Faq/Faq';
 
 const CoursePage = () => {
-  const [activeTab, setActiveTab] = useState('content'); // 'intro' hoặc 'content'
+    const [activeTab, setActiveTab] = useState('content'); // 'intro' hoặc 'content'
 
-  return (
-    <div className="course-container">
-      <div className="course-tabs">
-        <button
-          className={`tab-button ${activeTab === 'intro' ? 'active' : ''}`}
-          onClick={() => setActiveTab('intro')}
-        >
-          Giới Thiệu
-        </button>
-        <button
-          className={`tab-button ${activeTab === 'content' ? 'active' : ''}`}
-          onClick={() => setActiveTab('content')}
-        >
-          Nội dung
-        </button>
-      </div>
+    return (
+        <div className="course-container">
+            <div className="course-tabs">
+                <button
+                    className={`tab-button ${activeTab === 'intro' ? 'active' : ''}`}
+                    onClick={() => setActiveTab('intro')}
+                >
+                    Giới Thiệu
+                </button>
+                <button
+                    className={`tab-button ${activeTab === 'content' ? 'active' : ''}`}
+                    onClick={() => setActiveTab('content')}
+                >
+                    Nội dung
+                </button>
+            </div>
 
-      <div className="course-content">
-        {activeTab === 'intro' ? <CourseIntro /> : <CourseContent />}
-      </div>
-    </div>
-  );
+            <div className="course-content">
+                {activeTab === 'intro' ? <CourseIntro /> : <CourseContent />}
+            </div>
+
+            <Faq />
+        </div>
+    );
 };
 
 export default CoursePage;
